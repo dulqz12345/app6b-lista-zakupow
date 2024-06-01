995f17a59ab8e53452249d79881069c9b3584b2a
Dulqz <you@example.com>
2024-06-01 09:53:46 +0200

    Możliwość zaznaczenia produktu Moniki/Jacka/wspólnych

--- app.py
+++ app.py
@@ -10,27 +10,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-
 @app.before_first_request
 def create_tables():
     db.create_all()
-
 
 @app.route('/')
 def index():
     shopping_list = ShoppingItem.query.all()
     return render_template('index.html', shopping_list=shopping_list)
 
-
 @app.route('/add', methods=['POST'])
 def add():
     item_name = request.form.get('item')
+    is_monika = 'is_monika' in request.form
+    is_jacek = 'is_jacek' in request.form
     if item_name:
-        new_item = ShoppingItem(name=item_name)
+        new_item = ShoppingItem(name=item_name, is_monika=is_monika, is_jacek=is_jacek)
         db.session.add(new_item)
         db.session.commit()
     return redirect(url_for('index'))
-
 
 @app.route('/delete/<int:item_id>', methods=['POST'])
 def delete(item_id):
@@ -38,7 +36,6 @@ def delete(item_id):
     db.session.delete(item)
     db.session.commit()
     return redirect(url_for('index'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -4,6 +4,8 @@ from db import db
 class ShoppingItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    is_monika = db.Column(db.Boolean, default=False)
+    is_jacek = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return f'<ShoppingItem {self.name}>'
